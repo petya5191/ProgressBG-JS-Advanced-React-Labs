69a4f0d8361a12368755515d53af9750360da57b
WWWCourses <ivawebcourses@gmail.com>
2020-10-13 22:59:03 +0300

    Lab1: 13.10.2020 DONE: till https://wwwcourses.github.io/ProgressBG-JS-Advanced-React-Slides/pages/themes/JS_Overview/JS_Overview.html#/6

---
+++ lab1/tmp.js
@@ -0,0 +1,20 @@
+// function foo() {
+// 	x = 5;
+// }
+
+// foo();
+
+// console.log(x);
+
+
+// if( 1 ){
+// 	console.log(1);
+// };
+
+
+// console.log( 2 == '2' ); // true
+// console.log( 2 === '2' ); // false
+
+console.log( '3' > 'hello' );
+
+// CTRL+SHIFT+B
