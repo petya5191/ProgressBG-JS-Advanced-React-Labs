93ed48948a6012d24b823cf34483705f47918f00
WWWCourses <ivawebcourses@gmail.com>
2020-10-28 12:56:37 +0200

    pre: Lab6

---
+++ .optional_topics/ASI_demo.js
@@ -0,0 +1,15 @@
+// function foo(x) {
+// 	return x;
+// }
+
+// console.log( foo(2+3) );
+
+// let res = foo
+// (2+3)*4;
+// console.log(res);
+
+let y = '1000';
+let x = 3
++y
+console.log(x);
+console.log(y);
